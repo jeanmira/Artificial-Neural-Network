175c69ea425af91064f7f1827c47d833f2acdf64
Jean Marcelo MIra Junior <jeandemira@gmail.com>
2021-04-22 16:21:17 -0300

    Init

---
+++ biblioteca.py
@@ -0,0 +1,16 @@
+# ------------------------------- /usr/bin/g++-7 ------------------------------#
+# ------------------------------- coding: utf-8 -------------------------------#
+# Criado por:   Jean Marcelo Mira Junior
+# Versão: 1.0
+# Criado em: 22/05/2021
+# Sistema operacional: Linux - Ubuntu 20.04.1 LTS
+# Python 3
+# ------------------------------ Pacotes --------------------------------------#
+import os
+import csv
+import numpy as np
+
+from pybrain3.datasets import SupervisedDataSet
+from pybrain3.tools.shortcuts import buildNetwork
+from pybrain3.supervised.trainers import BackpropTrainer
+# -----------------------------------------------------------------------------#
